4e039d0c22b23a5387c069f2c4d39a3052594078
Moinak Dey <moinakdey21@gmail.com>
2024-07-14 21:38:24 +0530

    App

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+ 
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body : Stack(
+        children: [
+                const RiveAnimation.asset("assets/Riveassets/new_file.riv"),
+                Positioned.fill(child: 
+                BackdropFilter(filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
+                child: const SizedBox(),
+                ))
+        ],
+      )
+    );
+  }
+}
