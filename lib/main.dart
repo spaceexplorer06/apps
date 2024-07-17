59e6cb09d187285bc40b69abc0217848a26725b4
Moinak Dey <moinakdey21@gmail.com>
2024-07-18 02:39:31 +0530

    Changes done

--- lib/main.dart
+++ lib/main.dart
@@ -29,95 +29,94 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.brown[200],
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(65.0),
-            child: Image.asset('assets/Images/hello_logo.png'),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.email,
-                    color: Colors.brown[700],
-                  ),
-                  hintText: "E M A I L",
-                  hintStyle: TextStyle(color: Colors.brown[700]),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(color: Colors.brown, width: 2),
-                  ),
-                ),
+            resizeToAvoidBottomInset: false,
+            backgroundColor: Colors.brown[200],
+            body: Column(children: [
+              Padding(
+                padding: const EdgeInsets.all(65.0),
+                child: Image.asset('assets/Images/hello_logo.png'),
               ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                obscureText: _obscuretext,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.lock,
-                    color: Colors.brown[700],
-                  ),
-                  suffixIcon: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        _obscuretext = !_obscuretext;
-                      });
-                    },
-                    child: Icon(
-                      _obscuretext ? Icons.visibility : Icons.visibility_off,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.email,
                       color: Colors.brown[700],
                     ),
-                  ),
-                  hintText: "P A S S W O R D",
-                  hintStyle: TextStyle(color: Colors.brown[700]),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: const BorderSide(color: Colors.brown, width: 5),
+                    hintText: "E M A I L",
+                    hintStyle: TextStyle(color: Colors.brown[700]),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide:
+                          const BorderSide(color: Colors.brown, width: 2),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: Expanded(
-              child: Row(
-                children: [
-                  Text(
-                    "Don't have am account? ",
-                    style: TextStyle(fontSize: 20, color: Colors.brown[700]),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  obscureText: _obscuretext,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.brown[700],
+                    ),
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _obscuretext = !_obscuretext;
+                        });
+                      },
+                      child: Icon(
+                        _obscuretext
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Colors.brown[700],
+                      ),
+                    ),
+                    hintText: "P A S S W O R D",
+                    hintStyle: TextStyle(color: Colors.brown[700]),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide:
+                          const BorderSide(color: Colors.brown, width: 5),
+                    ),
                   ),
-                  const Text(
-                    "Sign Up",
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Text(
-              "Forgot Password",
-              style: TextStyle(fontSize: 20, color: Colors.brown[700]),
-            ),
-          ),
-          Expanded(
-              child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Expanded(
+                  child: Row(
+                    children: [
+                      Text(
+                        "Don't have am account? ",
+                        style:
+                            TextStyle(fontSize: 20, color: Colors.brown[700]),
+                      ),
+                      const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text(
+                  "Forgot Password",
+                  style: TextStyle(fontSize: 20, color: Colors.brown[700]),
+                ),
+              ),
+              Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ElevatedButton(
                 style: ButtonStyle(
                     backgroundColor:
                         MaterialStatePropertyAll(Colors.brown[900]),
@@ -134,57 +133,12 @@ class _LoginScreenState extends State<LoginScreen> {
                     )
                   ],
                 )),
-          )),
-          const Expanded(
-              child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(Colors.black),
-                    elevation: MaterialStatePropertyAll(50)),
-                onPressed: null,
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(12.0),
-                      child: Icon(
-                        Icons.g_mobiledata,
-                        color: Colors.orange,
-                      ),
-                    ),
-                    Text(
-                      "Sign In with Google",
-                      style: TextStyle(color: Colors.orange),
-                    )
-                  ],
-                )),
-          )),
-          Expanded(
-              child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-                style: const ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(Colors.black),
-                    elevation: MaterialStatePropertyAll(50)),
-                onPressed: null,
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Icon(
-                        Icons.facebook,
-                        color: Colors.blue[500],
-                      ),
-                    ),
-                    const Text(
-                      "Sign In with Facebook",
-                      style: TextStyle(color: Colors.blue),
-                    )
-                  ],
-                )),
-          ))
-        ],
-      ),
-    ));
+                            ),
+              Row(children: [
+                Icon(Icons.g_mobiledata),
+              ],)
+            ],),
+            )
+            );
   }
 }
