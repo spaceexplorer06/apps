befc0c1b33fb19efa15afdb1f0e6007979beae7d
Moinak Dey <moinakdey21@gmail.com>
2024-07-25 22:22:32 +0530

    done

--- lib/ViewScreen.dart
+++ lib/ViewScreen.dart
@@ -21,25 +21,27 @@ class _ViewScreenState extends State<ViewScreen> {
     return Scaffold(
         appBar: AppBar(actions: [
       PopupMenuButton<MenuAction>(
-          shadowColor: const Color.fromARGB(255, 5, 1, 1),
-          elevation: 20,
-          color: const Color.fromARGB(255, 208, 204, 197),
-          onSelected: (value) async {
-            switch (value) {
-              case MenuAction.Logout:
-                final shouldLogout = await showlogoutDialog(context);
-                if (shouldLogout) {
-                  await FirebaseAuth.instance.signOut();
-                  Navigator.of(context)
-                      .pushNamedAndRemoveUntil('/login/', (_) => false);
-                }
-                ;
-            }
-          }, itemBuilder: (context) {
-            return const [
-              PopupMenuItem(value: MenuAction.Logout,child: Text('Logout'))
-            ];
-          },),
+        shadowColor: const Color.fromARGB(255, 5, 1, 1),
+        elevation: 20,
+        color: const Color.fromARGB(255, 208, 204, 197),
+        onSelected: (value) async {
+          switch (value) {
+            case MenuAction.Logout:
+              final shouldLogout = await showlogoutDialog(context);
+              if (shouldLogout) {
+                await FirebaseAuth.instance.signOut();
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil('/login/', (_) => false);
+              }
+          }
+        },
+        itemBuilder: (context) {
+          return const [
+            PopupMenuItem(value: MenuAction.Logout, child: Text('Logout'))
+          ];
+        },
+      ),
+      
     ]));
   }
 }
