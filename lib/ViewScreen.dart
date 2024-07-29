3f7ed8dd55e6579d26c8e0a6695961c5e6c0a68a
Moinak Dey <moinakdey21@gmail.com>
2024-07-29 17:46:56 +0530

    done

--- lib/ViewScreen.dart
+++ lib/ViewScreen.dart
@@ -1,7 +1,6 @@
 import 'package:app/Account.dart';
 import 'package:app/Home.dart';
 import 'package:app/Settings_app.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 import 'package:app/Account.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:app/ViewScreen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
