88afea6a408c757a4bec94b272449f6826612ee8
Moinak Dey <moinakdey21@gmail.com>
2024-07-30 23:46:29 +0530

    done

--- lib/California.dart
+++ lib/California.dart
@@ -11,8 +11,12 @@ class California extends StatefulWidget {
 class _CaliforniaState extends State<California> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 195, 241, 197),
+    return Scaffold(
+      appBar: AppBar(
+        bottom: const PreferredSize(preferredSize: Size.fromHeight(200), child: Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.start,)),
+        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomLeft : Radius.circular(600))),
+      ),
+      backgroundColor: const Color.fromARGB(255, 195, 241, 197),
     );
   }
 }
