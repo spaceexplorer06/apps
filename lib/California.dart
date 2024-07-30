6db6e2d254362d740ea4ae00fef4ab7d05d09992
Moinak Dey <moinakdey21@gmail.com>
2024-07-31 00:57:35 +0530

    done

--- lib/California.dart
+++ lib/California.dart
@@ -17,6 +17,19 @@ class _CaliforniaState extends State<California> {
         bottom: const PreferredSize(preferredSize: Size.fromHeight(200), child: Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.start,)),
         shape: const RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomLeft : Radius.circular(600))),
       ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(vertical: 5),
+        child: Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text('Join the chat :',style: TextStyle(fontSize: 20),),
+                IconButton(onPressed: null, icon: Icon(Icons.chat,color: Colors.black,))
+              ],
+            )
+          ],
+        ),
+      ),
       backgroundColor: const Color.fromARGB(255, 195, 241, 197),
     );
   }
